64e629750b8318b31df39fc6b51865761f8f61c4
MarcelaDelgado <celeste1@hotmail.co.nz>
2018-09-29 14:42:01 +1200

    test5

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -7,8 +7,7 @@ public class PartD {
 		 System.out.println("Hi, my name is Marcela");
 		 System.out.println("My favourite colour is blue");
 		 
-		 System.out.println("My favourite mode of transportation is by car");
-			
+		 System.out.println("My favourite mode of transportation is by car");			
 		 System.out.println("The best day of the week is Saturday");
 		 
 
